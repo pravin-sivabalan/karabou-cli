5e0288a1c7bddb9e522f16e9ecc898a8a42d5c29
Pravin Sivabalan <ps@Pravins-MacBook-Pro.local>
2025-06-30 19:27:41 -0400

    update url

--- tap/Formula/karabou.rb
+++ tap/Formula/karabou.rb
@@ -1,8 +1,8 @@
 class Karabou < Formula
   desc "CLI for managing Karabiner Elements Complex Modifications"
   homepage "https://github.com/pravin-sivabalan/karabou-cli"
-  url "https://github.com/pravin-sivabalan/karabou-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "YOUR_SHA256_HERE"
+  url "https://github.com/pravin-sivabalan/karabou-cli/archive/refs/tags/v0.0.50.0.50.0.50.0.50.0.50.0.50.0.50.0.50.0.5.tar.gz"
+  sha256 "8929ca18f8e4929ff7460bf68aa4ab14e458d7676fa613c6e9bddde1190de48e"
   license "MIT"
 
   depends_on :xcode => ["14.0", :build]
