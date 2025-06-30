01cafcc1273b09b41f5e9bd27f42bcdc270efd88
Pravin Sivabalan <ps@Pravins-MacBook-Pro.local>
2025-06-30 19:22:10 -0400

    update sha256

--- Formula/karabou.rb
+++ Formula/karabou.rb
@@ -2,7 +2,7 @@ class Karabou < Formula
   desc "CLI for managing Karabiner Elements Complex Modifications"
   homepage "https://github.com/pravin-sivabalan/karabou-cli"
   url "https://github.com/pravin-sivabalan/karabou-cli/releases/download/v1.0.0/KarabouCLI"
-  sha256 "YOUR_BINARY_SHA256_HERE"
+  sha256 "8929ca18f8e4929ff7460bf68aa4ab14e458d7676fa613c6e9bddde1190de48e"
   license "MIT"
 
   depends_on :macos
